ae932f1c5d3e8fced1677b89629062a18828c862
Kanjanapong <momonokuma78@gmail.com>
2017-09-15 17:29:51 +0700

    move calculate from sever to netflow, add percent

--- netflow.js
+++ netflow.js
@@ -1,18 +1,127 @@
 import moment from 'moment'
 import { reConvertDecimal } from './utilize'
+import { uniqBy } from 'lodash'
 
-export const propsNumber = [
+const propsNumber = [
   'cf_principal', 'cf_interest', 'cf_fee',
 ]
 
-export const propsBucket = [
+const propsBucket = [
   'b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10', 'b11', 'b12'
 ]
 
-export const propsOSB = [
+const propsOSB = [
   'loan_id',
   'trans_date',
   'trc',
   ...propsNumber,
   ...propsBucket,
 ]
+
+const propsTRC = [
+  'LO',
+  'PO',
+]
+
+const queryTransByDate = async (connection, start, end) => {
+  return new Promise(function(resolve, reject) {
+    connection.query(
+      `SELECT ${propsOSB} from Transaction 
+        WHERE (trans_date BETWEEN ? AND ?) AND trc != 'LO' AND trc != 'PO' 
+        ORDER BY trans_date DESC`,
+      [start, end],
+      function(err, rows, fields) {
+        if(!err){
+          resolve(rows)
+        } else {
+          reject(err)
+        }
+      }
+    )
+  })
+}
+
+const calculatePercent = async data => {
+  const result = []
+  let i = 0
+  while(i < data.length) {
+    const row = []
+    let percent = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+    let j = 0
+    while(j < data[i].length) {
+      if(j < 2) {
+        row.push(data[i][j])
+        if( j === 1) { // if finished insert TotalOSB and OSB
+          if (i > 0 && data[i-1][j-1] !== 0) percent[0] = data[i][j] / data[i-1][j-1] * 100
+          row.push(`${percent[0]}%`)
+        }
+      } else {
+        row.push(data[i][j])
+        if( i > 0 && data[i-1][j-1] !== 0) percent[j-1] = data[i][j] / data[i-1][j-1] * 100
+        row.push(`${percent[j-1]}%`)
+      }
+      j += 1
+    }
+    result.push(row)
+    i += 1
+  }
+  return result
+}
+
+export const riskNetflow = async (connection, date) => {
+  const result = []
+  const rawData = []
+  // count variable
+  let countMonth = 0
+  let maxLength = 0
+  let count = 0
+  while(countMonth < 13) { // while loop until 13 month
+    let totalOSB = 0
+    let OSB = 0
+    let bucket = [0,0,0,0,0,0,0,0,0,0,0,0]
+    // build time gap
+    let start = date.format("YYYY-MM-DD HH:mm:ss")
+    let end = date.add(1, 'month').format("YYYY-MM-DD HH:mm:ss")
+    //query Transaction and make unique by loan_id
+    const trans = uniqBy(await queryTransByDate(connection, start, end), 'loan_id')
+    maxLength = trans.length
+    let countTran = 0
+    // summary data by one transaction
+    while(countTran < maxLength) {
+      let temp = 0
+      // OSB calculate
+      propsNumber.map(prop => {
+        trans[countTran][`${prop}`] = reConvertDecimal(trans[countTran][`${prop}`])
+        temp += trans[countTran][`${prop}`]
+        return prop
+      })
+      // bucket calculate
+      propsBucket.map(prop => {
+        trans[countTran][`${prop}`] = reConvertDecimal(trans[countTran][`${prop}`])
+        return prop
+      })
+      while(count < bucket.length) {
+        bucket[count] += trans[countTran][`${propsBucket[count + 1]}`] // not include b0
+        totalOSB += trans[countTran][`${propsBucket[count + 1]}`]
+        count += 1
+      }
+      count = 0
+      OSB += temp
+      totalOSB += temp
+      countTran +=1
+    }
+    // push data to result
+    const arr = [totalOSB,OSB]
+    maxLength = bucket.length
+    count = 0
+    while(count < maxLength)
+    {
+      arr.push(bucket[count])
+      count+=1
+    }
+    count = 0
+    rawData.push(arr)
+    countMonth+=1
+  }
+  return await calculatePercent(rawData)
+}
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-import { riskNetflow, propsOSB, propsNumber, propsBucket } from './netflow'
-import { reConvertDecimal } from './utilize'
+import { riskNetflow } from './netflow'
 import moment from 'moment'
 import express from 'express'
 import mysql from 'mysql'
-import { uniqBy } from 'lodash'
 
 const app = express()
 
@@ -22,84 +20,10 @@ connection.connect(function(err){
   }
 })
 
-const queryTransByDate = async (start, end) => {
-  return new Promise(function(resolve, reject) {
-    connection.query(
-      `SELECT ${propsOSB} from Transaction 
-        WHERE (trans_date BETWEEN '${start}' AND '${end}') AND trc != 'LO' ORDER BY trans_date DESC`,
-      function(err, rows, fields) {
-        if(!err){
-          resolve(rows)
-        } else {
-          reject(err)
-        }
-      }
-    )
-  })
-}
-
 app.get("/netflow/:month/:year",async function(req, res){
-  const result = [] // export result
   let { year, month} = req.params // input param
-  // count variable
-  let countMonth = 0
-  let maxLength = 0
-  let count = 0
-  // make date backward 12 month
   const date = moment(`${year}${month}`, 'YYYYM').subtract(12, 'month')
-  while(countMonth < 4) { // while loop until 13 month
-    let totalOSB = 0
-    let OSB = 0
-    let bucket = [0,0,0,0,0,0,0,0,0,0,0,0]
-    // build time gap
-    let start = date.format("YYYY-MM-DD HH:mm:ss")
-    let end = date.add(1, 'month').format("YYYY-MM-DD HH:mm:ss")
-    //query Transaction and make unique by loan_id
-    const trans = uniqBy(await queryTransByDate(start, end), 'loan_id')
-    maxLength = trans.length
-    let countTran = 0
-    // summary data by one transaction
-    while(countTran < maxLength) {
-      let temp = 0
-      // OSB calculate
-      propsNumber.map(prop => {
-        trans[countTran][`${prop}`] = reConvertDecimal(trans[countTran][`${prop}`])
-        temp += trans[countTran][`${prop}`]
-        return prop
-      })
-      // bucket calculate
-      propsBucket.map(prop => {
-        trans[countTran][`${prop}`] = reConvertDecimal(trans[countTran][`${prop}`])
-        return prop
-      })
-      while(count < bucket.length) {
-        bucket[count] = trans[countTran][`${propsBucket[count + 1]}`] // not include b0
-        totalOSB += bucket[count]
-        count += 1
-      }
-      count = 0
-      OSB += temp
-      totalOSB += temp
-      countTran +=1
-    }
-    console.log(`TotalOSB = ${totalOSB}, OSB = ${OSB}, ${bucket}`)
-    // percent calculate
-    let percent = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-    // push data to result
-    const arr = [totalOSB,OSB]
-    maxLength = bucket.length
-    count = 0
-    while(count < maxLength)
-    {
-      arr.push(percent[count])
-      arr.push(bucket[count])
-      count+=1
-    }
-    arr.push(percent[count])
-    count = 0
-    result.push(arr)
-    countMonth+=1
-  }
+  const result = await riskNetflow(connection, date)
   res.send(result)
 })
 
